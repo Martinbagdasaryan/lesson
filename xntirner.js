00241d3aeb55e27cc47f1a0d965cea6e3028cec9
Martinbagdasaryan <bagdasaryan.martin00000@gmail.com>
2023-03-06 19:40:12 +0400

    xntirner

--- xntirner.js
+++ xntirner.js
@@ -1,87 +1,65 @@
 // 1
 
-function fib(num){
-    let c =[0,1,1]
-    let b = 1
-    let a =1
-    let sum = 0;
-    for (let i = 3; i < num;i++){
-        let g = b + a
-        b = a
-        a = g
-        c.push(g)
-    }
-    for(let j = 0;j <  num; j ++){
-        sum +=c[j]
-    }
-    console.log(c);
-    return sum
+function fib(num) {
+  let b = 1;
+  let a = 0;
+  let sum = 0;
+  for (let i = 0; i < num; i++) {
+    let temp = b + a;
+    b = a;
+    a = temp;
+    sum += a;
+  }
+  return sum;
 }
 
+// console.log(fib(6));
 //2
 
-function fact(num){
-    let c =[]
-    let q = []
-    let b = 1
-    for(let j = 1;j<=Math.floor(num/2);j ++){
-        c.push(j)
-        b = c.reduce((a, b) => a * b, 1)
-        q.push(b)
+const getFactorialOf = (num) => {
+  if (num <= 1) return num;
+  return num * getFactorialOf(num - 1);
+};
+
+function isFactorial(num) {
+  let multiple = 1;
+  let i = 1;
+  while (multiple <= num) {
+    multiple *= i++;
+    if (multiple === num) {
+      return true;
     }
-    for(let i = 0;i<q.length;i++){
-        if(q[i]===num){
-            return true
-        }
-    }
-    return false
-    // q.push(c.pop())
+  }
+  return false;
 }
 
 //3
 
-function pri(smnum,binum){
-    let q = []
-    if(31 > binum && binum >9){
-        for(smnum;smnum<binum+1;smnum++){
-            q.unshift(smnum)
-        }
-        return q
+function printReverse(smnum, binum) {
+  let arr = [];
+  if (31 > binum && binum > 9 && binum > smnum) {
+    for (smnum; smnum < binum + 1; smnum++) {
+      arr.unshift(smnum);
     }
-    else {
-        return "the entered number does not match the conditions"
+    return arr;
+  } else if (31 > smnum && smnum > 9 && binum < smnum) {
+    for (binum; binum < smnum + 1; binum++) {
+      arr.unshift(binum);
     }
+    return arr;
+  } else {
+    return "the entered number does not match the conditions";
+  }
 }
-
 //4
 
-function sum(number1,number2,number3){
-    let a = number1,b = number2,c = number3;
-    for(let i = 0;i<number1;i++){
-        a += i
-    }
-    console.log(a);
-    for(let i = 0;i<number2;i++){
-        b += i
-    }
-    console.log(b);
-    for(let i = 0;i<number3;i++){
-        c += i
-    }
-    console.log(c);
+let arr = [];
+function rec(n, m) {
+  if (n === m) {
+    return arr;
+  } else {
+    arr.push(m);
+    m--;
+    return rec(n, m);
+  }
 }
-
-//5
-
-let arr = []
-function rec(n,m){
-    if (n === m){
-        return arr
-    }
-    else{
-        arr.push(m)
-        m--
-        return rec(n,m)
-    }
-}
-
